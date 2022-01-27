f5c91b3c948b44fdc84f1ad9ca523231787b76bc
Morgan Roff <roffmorgan@gmail.com>
2022-01-27 23:13:17 +0100

    Simplify benchmark

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # wordle-solver
 An automated solver for the popular game: [wordle](https://www.powerlanguage.co.uk/wordle/)
+
+## Benchmark
+
+Engine benchmark result:
+
+```
+running 1 test
+test bench_guess_random_words ... bench:   4,947,665 ns/iter (+/- 269,893)
+
+test result: ok. 0 passed; 0 failed; 0 ignored; 1 measured; 0 filtered out; finished in 4.48s
+```
--- lib/benches/engine_bench.rs
+++ lib/benches/engine_bench.rs
@@ -2,7 +2,6 @@
 
 extern crate test;
 
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
@@ -11,15 +10,13 @@ use test::Bencher;
 use wordle_solver::*;
 
 #[bench]
-fn bench_guess_1000_words(b: &mut Bencher) -> Result<()> {
+fn bench_guess_random_words(b: &mut Bencher) -> Result<()> {
     let test_words = BufReader::new(File::open("../data/1000-words-shuffled.txt")?);
     let mut all_words = BufReader::new(File::open("../data/all-words-sorted.txt")?);
 
     let bank = WordBank::from_reader(&mut all_words)?;
 
-    let mut num_games_per_round: HashMap<u8, u32> = HashMap::new();
     let test_words: Vec<String> = test_words.lines().collect::<Result<Vec<String>>>()?;
-
     let mut test_word_iter = test_words.iter();
 
     b.iter(|| {
@@ -28,29 +25,9 @@ fn bench_guess_1000_words(b: &mut Bencher) -> Result<()> {
             test_word_iter = test_words.iter();
             test_word_iter.next().unwrap()
         });
-        let num_games = num_games_per_round
-            .entry(play_game(test_word, &bank))
-            .or_insert(0);
-        *num_games += 1;
+        let game = Game::new(&bank);
+        return game.play_game(test_word, u32::MAX);
     });
 
     Ok(())
-}
-
-fn play_game(word: &str, bank: &WordBank) -> u8 {
-    let mut game = Game::new(bank);
-
-    for round in 1..=255 {
-        let guess = game.calculate_best_guess().unwrap();
-        let result = get_result_for_guess(word, guess);
-
-        if result.letters.iter().all(|lr| match lr {
-            LetterResult::Correct(_) => true,
-            _ => false,
-        }) {
-            return round;
-        }
-        game.update_guess_result(&result);
-    }
-    return 255;
 }
