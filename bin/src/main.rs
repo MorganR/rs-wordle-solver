b69c4f1b1be8715cdbdca3f3c8ad873d7c626744
Morgan Roff <roffmorgan@gmail.com>
2022-01-27 19:39:10 +0100

    Read PossibleWords from file

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Result;
+use wordle_solver::*;
+
+/// Simple program to run a Wordle game in reverse, where the computer guesses the word.
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Args {
+    /// Path to a file that contains a list of possible words, with one word on each line.
+    #[clap(short = 'f', long)]
+    words_file: String,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    println!("File: {}", args.words_file);
+
+    let mut words_reader = BufReader::new(File::open(args.words_file)?);
+    let possible_words = PossibleWords::new(&mut words_reader)?;
+
+    println!("Read {} words.", possible_words.len());
+    Ok(())
 }
