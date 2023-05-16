af516b492a862841e0d51065e8bca537f469e2f2
Morgan Roff <roffmorgan@gmail.com>
2023-05-16 20:59:15 +0100

    Simplify MaxEliminations bench

--- lib/benches/engine_bench.rs
+++ lib/benches/engine_bench.rs
@@ -109,7 +109,7 @@ fn bench_max_approximate_eliminations_improved_words(b: &mut Bencher) -> Result<
 }
 
 #[bench]
-fn bench_max_eliminations_scorer_precomputed_improved_words(
+fn bench_max_eliminations_scorer_with_precomputed_improved_words(
     b: &mut Bencher,
 ) -> std::result::Result<(), Box<dyn Error>> {
     let test_words = io::BufReader::new(File::open("../data/1000-improved-words-shuffled.txt")?);
@@ -131,25 +131,14 @@ fn bench_max_eliminations_scorer_precomputed_improved_words(
 }
 
 #[bench]
-fn bench_max_eliminations_scorer_no_precompute_improved_words(
+fn bench_max_eliminations_scorer_precompute_improved_words(
     b: &mut Bencher,
 ) -> std::result::Result<(), Box<dyn Error>> {
-    let test_words = io::BufReader::new(File::open("../data/1000-improved-words-shuffled.txt")?);
     let mut all_words = io::BufReader::new(File::open("../data/improved-words.txt")?);
-
     let bank = WordBank::from_reader(&mut all_words)?;
 
-    let test_words: Vec<String> = test_words.lines().collect::<io::Result<Vec<String>>>()?;
-    let mut test_word_iter = test_words.iter().cycle();
-
     b.iter(|| {
-        let test_word = test_word_iter.next().unwrap();
-        let guesser = MaxScoreGuesser::new(
-            GuessFrom::AllUnguessedWords,
-            &bank,
-            MaxEliminationsScorer::new(&bank).unwrap(),
-        );
-        return play_game_with_guesser(test_word, 128, guesser);
+        return MaxEliminationsScorer::new(&bank);
     });
 
     Ok(())
